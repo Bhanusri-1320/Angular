64db0cf803f4961f702ade1118875ce4d903939f
Bhanusri-1320 <bhanusri.gangadevi@proclink.com>
2024-07-25 18:01:20 +0530

    deleting movies working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
+import { ColorGameComponent } from './color-game/color-game.component';
+import { HomeComponent } from './home/home.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'movies',
+    component: AppComponent,
+  },
+  {
+    path: 'color',
+    component: ColorGameComponent,
+  },
+];
